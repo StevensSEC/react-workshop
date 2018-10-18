8a3353f25f9745afb623270d623206be3efc76db
Blake Zimmerman <blake.zimmerman@icloud.com>
2018-10-17 23:01:00 -0400

    Add contents of workshop

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Header } from "./Header";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    // Initialize state
+    this.state = {
+      inputValue: ""
+    };
+  }
+
+  inputElem = null; // Declare variable to hold ref
+
+  componentDidMount() {
+    // Focus input element after component renders
+    this.inputElem.focus();
+  }
+
+  handleInput = event => {
+    // Set state to value of input element
+    this.setState({
+      inputValue: event.currentTarget.value
+    });
+  };
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+        <Header color={"red"}>
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        </Header>
+        <input
+          type="text"
+          ref={inputRef => (this.inputElem = inputRef)}
+          value={this.state.inputValue}
+          onChange={this.handleInput}
+        />
+        <div>{this.state.inputValue}</div>
       </div>
     );
   }
